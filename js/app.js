364e7743f4e5abd934126e853185138afc9d698e
dodypras <prasetyodody17@gmail.com>
2019-04-29 05:50:03 +0800

    add functionality for mobile navbar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 window.addEventListener("DOMContentLoaded",function() {
-	console.log('Javascript loaded!');
-}
+	var mobileNavToggle = document.getElementById('mobile-nav-toggle');
+
+	if (window.getComputedStyle(mobileNavToggle).display === 'block') {
+		mobileNavToggle.addEventListener('click', function() {
+			toggleNavbar();
+		});
+	}
+});
+
+function toggleNavbar() {
+	var mobileNavOpen = document.getElementById('open');
+	var mobileNavClose = document.getElementById('close');
+	var mobileNav = document.getElementById('mobile-nav');
+
+	if (window.getComputedStyle(mobileNav).left === '-260px') {
+		mobileNavOpen.style.display = 'none';
+		mobileNavClose.style.display = 'block';
+		mobileNav.style.left = '0';
+	}else{
+		mobileNavOpen.style.display = 'block';
+		mobileNavClose.style.display = 'none';
+		mobileNav.style.left = '-260px';
+	}
+};
